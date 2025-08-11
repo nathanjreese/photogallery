26e6793c906c44100dbaedccfca38d6fd7d7b82d
Nathan <nathanjreese@gmail.com>
2025-08-11 18:47:37 -0400

    commit fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Gallery from './Photos'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
