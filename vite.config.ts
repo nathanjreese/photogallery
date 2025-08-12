1dab39c5246cbbf4ea7507122c2ef9bc826d1aa8
Nathan <nathanjreese@gmail.com>
2025-08-12 18:28:33 -0400

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
+  base: '/photogallery',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
