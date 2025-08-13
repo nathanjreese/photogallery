979c486a40fcc57df428ba9b700a74d3305326ec
Nathan <nathanjreese@gmail.com>
2025-08-12 20:54:44 -0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-// import Gallery from './Photos'
+import Gallery from './Photos'
 import './App.css'
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      {/* <Gallery /> */}
+      <Gallery />
     </>
   )
 }
