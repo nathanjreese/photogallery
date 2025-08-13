44d8bc25f154e8c9faf809722f11d51e17bad59b
Nathan <nathanjreese@gmail.com>
2025-08-12 20:46:11 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/photogallery/' : '/',
+export default defineConfig({
+  base: '/photogallery',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
     },
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-    },
-  },
-}));
+  }
+});
